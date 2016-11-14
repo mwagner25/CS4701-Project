e168271aa7709055bc38a88a68b467e0bd8e53e2
jbyman <jakebyman@yahoo.com>
2016-11-14 01:54:35 -0500

    Restructured classes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,7 @@
 package application;
 	
 import java.io.FileInputStream;
-
+import board.Grid;
 import board.Tile;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -14,46 +14,17 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 
-
-
 public class Main extends Application {
-	
-	private Parent createContent(){
-		Pane root = new Pane();
-		root.setPrefSize(600, 600);
-		
-		for(int i = 0; i < 3; i++){
-			for(int g = 0; g < 3; g++){
-				Tile t = new Tile();
-				t.setTranslateX(g * 200);
-				t.setTranslateY(i * 200);
-				
-				root.getChildren().add(t);
-			}
-		}
-		
-		return root;
-	}
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-//	        TilePane grid = new TilePane();
-//	        grid.setPrefColumns(5);
-//	        grid.setPrefRows(10);
-//	        grid.setStyle("-fx-background-color: Black;");
-//	        FileInputStream input = new FileInputStream("assets/png/cat_cell.png");
-//	        Image cat = new Image(input);
-//	        ImageView imageView = new ImageView(cat);
-//	        HBox hbox = new HBox(imageView);
-//			BorderPane root = new BorderPane();
-			Scene scene = new Scene(createContent(),2000,1000);
+			Grid grid = new Grid(5, 5);
+			Scene scene = new Scene(grid.getGrid(),2000,1000);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Darwin's Nightmare");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
-			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- src/board/Grid.java
+++ src/board/Grid.java
@@ -1,5 +1,57 @@
 package board;
 
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+
 public class Grid {
+	
+	// Variable to represent the grid
+	private Pane grid;
+	
+	/*
+	 * Constructor to create a grid with a specified number of
+	 * rows and columns
+	 * 
+	 * @param rows - the number of rows
+	 * @param columns - the number of columns
+	 * 
+	 * */
+	public Grid(int rows, int columns){
+		this.grid = (Pane) generateGrid(rows, columns);
+	}
+	
+	/*
+	 * Function to generate the Pane of a specified number of 
+	 * rows and columns
+	 * 
+	 * @param rows - the number of rows
+	 * @param columns - the number of columns
+	 * 
+	 * @returns the Pane object
+	 * */
+	private Parent generateGrid(int rows, int columns){
+		Pane root = new Pane();
+		root.setPrefSize(600, 600);
+		
+		for(int i = 0; i < rows; i++){
+			for(int g = 0; g < columns; g++){
+				Tile t = new Tile();
+				t.setTranslateX(g * 200);
+				t.setTranslateY(i * 200);
+				
+				root.getChildren().add(t);
+			}
+		}
+		
+		return root;
+	}
+	
+	/*
+	 * Getter to return the grid
+	 * 
+	 * */
+	public Pane getGrid(){
+		return this.grid;
+	}
 
 }
